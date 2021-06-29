7f8a1b76dd085ee2ccb2ebbdbd6f0cebd6f2ddbf
Lex Nelson <lexinelson597@gmail.com>
2021-06-29 18:05:23 -0400

    commit tree bde0b372beb3344d2016165ece491ff81135a18d

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+const results = function(element) {
+    return (element.result === 'W')
+}
+function superbowlWin(array) {
+    const wins = array.find(results)
+    if (wins === undefined) {
+        return undefined;
+    } else {
+        return wins.year;
+    }
+}
